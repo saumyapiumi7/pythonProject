adafd53aaac3745a2f2e42aa7059c81c7dff71ac
Saumya Herath <saumyapiumi7@gmail.com>
2023-07-02 13:06:53 +0530

    create topic as user information

--- dataE.py
+++ dataE.py
@@ -1,5 +1,15 @@
 import tkinter
 
 window = tkinter.Tk()
+window.title("Data Entry Form")
+
+frame = tkinter.Frame(window)
+frame.pack()
+
+#user information
+user_info_frame = tkinter.LabelFrame(frame, text="User Information")
+user_info_frame.grid(row=0, column=0)
+
+
 
 window.mainloop()
