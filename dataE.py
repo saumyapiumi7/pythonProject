df625cec885860cc4fab39de10f0e8523c43342d
Saumya Herath <saumyapiumi7@gmail.com>
2023-07-02 13:07:29 +0530

    create name lable

--- dataE.py
+++ dataE.py
@@ -10,6 +10,12 @@ frame.pack()
 user_info_frame = tkinter.LabelFrame(frame, text="User Information")
 user_info_frame.grid(row=0, column=0)
 
+first_name_label = tkinter.Label(user_info_frame, text="First Name")
+first_name_label.grid(row=0, column=0)
+
+last_name_label = tkinter.Label(user_info_frame, text="Last Name")
+last_name_label.grid(row=0, column=1)
+
 
 
 window.mainloop()
