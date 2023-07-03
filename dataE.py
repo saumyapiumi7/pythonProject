2f726b2c1976295d54f1126b518720d932fc3848
Saumya Herath <saumyapiumi7@gmail.com>
2023-07-03 14:33:15 +0530

    create name entrys

--- dataE.py
+++ dataE.py
@@ -16,6 +16,9 @@ first_name_label.grid(row=0, column=0)
 last_name_label = tkinter.Label(user_info_frame, text="Last Name")
 last_name_label.grid(row=0, column=1)
 
-
+first_name_entry = tkinter.Entry(user_info_frame)
+last_name_entry = tkinter.Entry(user_info_frame)
+first_name_entry.grid(row=1, column=0)
+last_name_entry.grid(row=1, column=1)
 
 window.mainloop()
