904305dc26edcf6b248a1d30053ea3495c38c799
Saumya Herath <saumyapiumi7@gmail.com>
2023-09-01 10:32:09 +0530

    add the person's title lable and combpbox

--- dataE.py
+++ dataE.py
@@ -1,4 +1,5 @@
 import tkinter
+from tkinter import ttk
 
 window = tkinter.Tk()
 window.title("Data Entry Form")
@@ -8,7 +9,7 @@ frame.pack()
 
 #user information
 user_info_frame = tkinter.LabelFrame(frame, text="User Information")
-user_info_frame.grid(row=0, column=0)
+user_info_frame.grid(row=0, column=0, padx=20, pady=20)
 
 first_name_label = tkinter.Label(user_info_frame, text="First Name")
 first_name_label.grid(row=0, column=0)
@@ -20,5 +21,12 @@ first_name_entry = tkinter.Entry(user_info_frame)
 last_name_entry = tkinter.Entry(user_info_frame)
 first_name_entry.grid(row=1, column=0)
 last_name_entry.grid(row=1, column=1)
+
+title_label=tkinter.Label(user_info_frame, text="Title")
+title_combobox = ttk.Combobox(user_info_frame, values=["","Mr","Miss","Mrs","Dr"])
+title_label.grid(row=0, column=2)
+title_combobox.grid(row=1, column=2)
+
+                              
 
 window.mainloop()
