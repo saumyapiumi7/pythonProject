38adcf166c2ce502d48f98c39dbe703e745e7116
Saumya Herath <saumyapiumi7@gmail.com>
2023-09-01 17:52:39 +0530

    file path change

--- dataE.py
+++ dataE.py
@@ -31,7 +31,7 @@ def enter_data():
             print("Registration Status : " + registration_status)
             print("================================================================")
 
-            filepath = "D:\Project\data.xlsx"
+            filepath = "D:\Projects\Python\Form\data.xlsx"
 
             if not os.path.exists(filepath):
                 workbook = openpyxl.Workbook()
