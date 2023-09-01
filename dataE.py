71365ec58d18c59541365f52c97157d98d96faf0
Saumya Herath <saumyapiumi7@gmail.com>
2023-09-01 12:52:00 +0530

    add the terms and contition checkbox

--- dataE.py
+++ dataE.py
@@ -62,8 +62,13 @@ numsemesters_spinbox.grid(row=1, column= 2)
 for widget in courses_frame.winfo_children():
     widget.grid_configure(padx=10, pady=5)
 
+terms_frame = tkinter.LabelFrame(frame, text="Terms & Conditions")
+terms_frame.grid(row=2, column=0, sticky="news", padx=20, pady=20)
+
+terms_check = tkinter.Checkbutton(terms_frame, text="I accept the terms and conditions")
+terms_check.grid(row=0, column=0)
 
 
-    
+
 
 window.mainloop()
